1ea38ad922a653f48c6946cf1846571e543abc43
Martin M Reed <martin.reed@hallowtech.com>
2011-11-07 20:24:52 -0600

    8:24PM

--- src/main/java/org/hardisonbrewing/maven/cxx/qnx/InitializeMojo.java
+++ src/main/java/org/hardisonbrewing/maven/cxx/qnx/InitializeMojo.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2010-2011 Martin M Reed
+ * Copyright (c) 2011 Martin M Reed
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
