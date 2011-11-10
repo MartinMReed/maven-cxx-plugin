f36ee94812437e1821f8de17d72b77198aba34e2
Martin M Reed <martin.reed+github.user@hallowtech.com>
2011-11-09 20:12:35 -0600

    8:12PM

--- src/main/java/org/hardisonbrewing/maven/cxx/qnx/CompileMojo.java
+++ src/main/java/org/hardisonbrewing/maven/cxx/qnx/CompileMojo.java
@@ -90,7 +90,7 @@ public class CompileMojo extends JoJoMojoImpl {
             Commandline commandLine = buildCommandline( cmd );
             CommandLineService.appendEnvVar( commandLine, "PATH", QnxService.getQnxHostBinPath() );
             commandLine.addEnvironment( "QNX_HOST", QnxService.getQnxHostDirPath() );
-            commandLine.addEnvironment( "QNX_TARGET", QnxService.getQnxTargetPath() );
+            commandLine.addEnvironment( "QNX_TARGET", QnxService.getQnxTargetDirPath() );
             execute( commandLine );
         }
     }
