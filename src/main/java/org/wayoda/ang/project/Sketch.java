291296fc838dc0581f0bd3f4b7403b9000107ea3
Martin M Reed <martin.reed@hallowtech.com>
2011-11-29 14:24:32 -0600

    2:24PM

--- src/main/java/org/wayoda/ang/project/Sketch.java
+++ src/main/java/org/wayoda/ang/project/Sketch.java
@@ -33,6 +33,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
+import org.hardisonbrewing.maven.core.JoJoMojo;
 import org.hardisonbrewing.maven.cxx.TargetDirectoryService;
 import org.wayoda.ang.libraries.CodeManager;
 import org.wayoda.ang.libraries.Library;
